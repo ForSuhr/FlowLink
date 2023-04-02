ec45a120a60d484805ec794291c4cbc5a60b5cea
ForSuhr <jiandong.zhao.zier@gmail.com>
2023-04-02 21:54:33 +0200

    Add ShareAddress mode to the socket in the sender

--- desktop/Sender.cpp
+++ desktop/Sender.cpp
@@ -11,7 +11,7 @@ Sender::Sender(QObject *parent)
 void Sender::sendDatagram()
 {
     QUdpSocket udpSocketIPv4;
-    udpSocketIPv4.bind(QHostAddress("0.0.0.0"));
+    udpSocketIPv4.bind(QHostAddress::AnyIPv4, 8080, QUdpSocket::ShareAddress);
     // set link-local as multicast scope when the TTL of the IPv4 socket is set to 1
     // multicast packet is sent to the multicast group address in the range 224.0.0.0/24
     udpSocketIPv4.setSocketOption(QAbstractSocket::MulticastTtlOption, 1);
