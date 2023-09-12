8a47d9ddff3ab61a5f169290d3744329795a3c8a
ForSuhr <jiandong.zhao.zier@gmail.com>
2023-09-13 00:56:53 +0200

    UI: add a inline function to convert bytes to other units

---
+++ desktop/windows/WindowsGlobals.cpp
@@ -0,0 +1,1 @@
+#include "WindowsGlobals.h"
---
+++ desktop/windows/WindowsGlobals.h
@@ -0,0 +1,41 @@
+#ifndef WINDOWS_GLOBALS_H
+#define WINDOWS_GLOBALS_H
+
+#include <QtEndian>
+#include <QString>
+
+struct ConvertedNumber
+{
+    double number;
+    QString unit;
+};
+
+inline ConvertedNumber BytesConvert(qint64 bytes)
+{
+    ConvertedNumber result;
+
+    if (bytes < 1024)
+    {
+        result.number = static_cast<qreal>(bytes);
+        result.unit = "B";
+    }
+    else if (bytes < 1024 * 1024)
+    {
+        result.number = static_cast<qreal>(bytes) / 1024;
+        result.unit = "kB";
+    }
+    else if (bytes < 1024 * 1024 * 1024)
+    {
+        result.number = static_cast<qreal>(bytes) / (1024 * 1024);
+        result.unit = "MB";
+    }
+    else
+    {
+        result.number = static_cast<qreal>(bytes) / (1024 * 1024 * 1024);
+        result.unit = "GB";
+    }
+
+    return result;
+}
+
+#endif // WINDOWS_GLOBALS_H
