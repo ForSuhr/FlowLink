7211181b1a2af681514cadaf1f2d1f272d7dd735
ForSuhr <jiandong.zhao.zier@gmail.com>
2023-09-14 19:12:37 +0200

    UI: progress bar value binding

--- desktop/windows/ProgressWindow.cpp
+++ desktop/windows/ProgressWindow.cpp
@@ -1,11 +1,13 @@
 #include "ProgressWindow.h"
 
-ProgressWindow::ProgressWindow(QWidget *parent) : QWidget(parent)
+ProgressWindow::ProgressWindow(QWidget *parent) : QWidget(parent),
+                                                  m_progressWidgetMap(new std::unordered_map<QString, QWidget *>)
 {
     setupUI();
-    createProgressWidget("a.out", 10LL);
-    createProgressWidget("b.out", 3000 * 1024LL);
-    createProgressWidget("c.out", 2000LL);
+}
+
+ProgressWindow::~ProgressWindow()
+{
 }
 
 void ProgressWindow::setupUI()
@@ -19,17 +21,22 @@ QVBoxLayout *ProgressWindow::layout()
     return m_vbox;
 }
 
-void ProgressWindow::createProgressWidget(const QString &filename, qint64 fileTotalBytes)
+void ProgressWindow::createProgressWidget(const QString &filename, qint64 totalFileBytes)
 {
     // widgets
-    QWidget *progressWidget = new QWidget(this);
+    QWidget *progressWidget = new QWidget();
     progressWidget->setObjectName("progressWidget");
     progressWidget->setFixedHeight(64);
-    QLabel *fileName = new QLabel(filename);
-    ConvertedNumber fileTotalSize = BytesConvert(fileTotalBytes);
-    fileCurrentSizeLabel = new QLabel("0GB");
+    QLabel *filenameLabel = new QLabel(filename);
+    ConvertedNumber fileTotalSize = BytesConvert(totalFileBytes);
+    QLabel *fileCurrentSizeLabel = new QLabel("0GB");
+    fileCurrentSizeLabel->setObjectName("fileCurrentSizeLabel");
     QLabel *fileTotalSizeLabel = new QLabel(QString("/%1").arg(QString::number(fileTotalSize.number, 'f', 2) + fileTotalSize.unit));
-    progressBar = new QProgressBar();
+    QProgressBar *progressBar = new QProgressBar();
+    progressBar->setObjectName("progressBar");
+    progressBar->setFixedHeight(5);
+    progressBar->setTextVisible(false);
+    progressBar->setMaximum(totalFileBytes);
 
     // layout
     QVBoxLayout *vbox = new QVBoxLayout(progressWidget);
@@ -39,14 +46,29 @@ void ProgressWindow::createProgressWidget(const QString &filename, qint64 fileTo
 
     // add widgets to layout
     m_vbox->insertWidget(m_vbox->count() - 1, progressWidget);
-    vbox->addWidget(fileName);
+    vbox->addWidget(filenameLabel);
     hbox->addWidget(fileCurrentSizeLabel);
     hbox->addWidget(fileTotalSizeLabel);
     hbox->addStretch();
     vbox->addLayout(hbox);
     vbox->addWidget(progressBar);
 
-    progressBar->setValue(50);
-    progressBar->setFixedHeight(5);
-    progressBar->setTextVisible(false);
+    // store the progress widget to a map for the convenience we query it
+    if (m_progressWidgetMap->find(filename) == m_progressWidgetMap->end())
+        (*m_progressWidgetMap)[filename] = progressWidget;
+}
+
+void ProgressWindow::updateProgress(const QString &filename, qint64 receivedBytes, qint64 totalBytes)
+{
+    // retrieve the progress widget from the map by filename
+    QWidget *progressWidget = (*m_progressWidgetMap)[filename];
+
+    // update the progress in number
+    QLabel *fileCurrentSizeLabel = progressWidget->findChild<QLabel *>("fileCurrentSizeLabel");
+    ConvertedNumber fileCurrentSize = BytesConvert(receivedBytes);
+    fileCurrentSizeLabel->setText(QString::number(fileCurrentSize.number, 'f', 2) + fileCurrentSize.unit);
+
+    // update the progress in progress bar
+    QProgressBar *progressBar = progressWidget->findChild<QProgressBar *>("progressBar");
+    progressBar->setValue(receivedBytes);
 }
--- desktop/windows/ProgressWindow.h
+++ desktop/windows/ProgressWindow.h
@@ -5,6 +5,9 @@
 #include <QBoxLayout>
 #include <QLabel>
 #include <QProgressBar>
+#include <QDebug>
+
+#include <memory>
 
 #include "WindowsGlobals.h"
 
@@ -14,17 +17,16 @@ class ProgressWindow : public QWidget
 
 public:
     explicit ProgressWindow(QWidget *parent = nullptr);
+    ~ProgressWindow();
 
     void setupUI();
     QVBoxLayout *layout();
-    void createProgressWidget(const QString &filename, qint64 fileTotalBytes);
-    void updateProgress(int bytes);
+    void createProgressWidget(const QString &filename, qint64 totalFileBytes);
+    void updateProgress(const QString &filename, qint64 bytes, qint64 totalBytes);
 
 private:
     QVBoxLayout *m_vbox = nullptr;
-    QLabel *fileCurrentSizeLabel = nullptr;
-    QProgressBar *progressBar = nullptr;
-    std::unordered_map<QString, QWidget *> m_progressWidgetMap = {}; // a map used to store progress widgets, one widget per downloading file
+    std::unique_ptr<std::unordered_map<QString, QWidget *>> m_progressWidgetMap = nullptr; // a map used to store progress widgets, one widget per downloading file
 };
 
 #endif // PROGRESS_WINDOW_H
